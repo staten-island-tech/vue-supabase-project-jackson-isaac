10cd4f9f73cc4c4718f0deb2f93fa195008c2586
jacksonz16 <jacksonz2390@gmail.com>
2024-05-08 09:06:08 -0400

    1

---
+++ vite-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://qjdheqsxsenztyvwtgnb.supabase.co', '<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZGhlcXN4c2VuenR5dnd0Z25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ1MzU5NTEsImV4cCI6MjAzMDExMTk1MX0.FCG3dbH_0tQyN9dL4ij2mSSyJcW_PDQQ4idj6LlmRj0>')
