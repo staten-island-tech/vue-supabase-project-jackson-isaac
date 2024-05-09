ee410ced15a08c2fdbf634087d40f8b3a5384056
jacksonz16 <jacksonz2390@gmail.com>
2024-05-09 08:57:00 -0400

    9

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,21 +1,21 @@
 <script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
-  const countries = ref([])
+const countries = ref([])
 
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
 
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
+onMounted(() => {
+  getCountries()
+})
+</script>
 
-  <template>
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
-  </template>
+<template>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+</template>
